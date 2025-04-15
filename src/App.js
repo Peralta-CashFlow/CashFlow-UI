cfb376aa3f64899355c0956924796f3a011ecb1c
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-04-15 11:21:51 -0300

    feat: adding login route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/login/Login';
+
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
