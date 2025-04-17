ed6bad21c9d61fba278d6908fe1d6dc0747411c3
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-04-17 12:44:20 -0300

    chore: create assets folder

---
+++ src/assets/colors/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    darkGray: "#212121",
+    gray: "#303030",
+    blueGreen: "#074471",
+    lightBlueGreen: "#247e7e",
+    darkBlueGreen: "#102030",
+    blue: "#105775"
+};
+
+export default colors;
