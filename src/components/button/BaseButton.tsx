6709663a79f08573e1cbc0906b562a2a9f734c4f
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-04-17 12:44:00 -0300

    feat: base button component

---
+++ src/components/button/BaseButton.tsx
@@ -0,0 +1,81 @@
+import { Button } from "@mui/material";
+import { ButtonProps } from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+import { SvgIconTypeMap } from "@mui/material/SvgIcon/SvgIcon";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from '@mui/material/Box';
+
+interface BaseButtonProps {
+    text: string;
+    variant?: ButtonProps['variant'];
+    icon?: OverridableComponent<SvgIconTypeMap>
+    backGroundColor?: string;
+    loading?: boolean;
+    color?: string;
+    className?: string;
+    fontSize?: string;
+    fontWeight?: string;
+    spinnerSize?: number;
+}
+
+const BaseButton: React.FC<BaseButtonProps> = ({
+    text, variant = 'contained', icon: Icon,
+    backGroundColor, loading, color = 'white',
+    className, fontSize, fontWeight, spinnerSize
+}) => {
+    return (
+        <div className={className}
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                position: 'relative',
+            }}
+        >
+            <Button
+                variant={variant}
+                endIcon={Icon && !loading ? <Icon /> : null}
+                sx={{
+                    fontSize: fontSize,
+                    fontWeight: fontWeight,
+                    backgroundColor: backGroundColor,
+                    width: '100%',
+                    height: '100%',
+                }}
+            >
+                {
+                    loading ?
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                width: '100%',
+                            }}
+                        >
+                            <CircularProgress
+                                size={spinnerSize}
+                                sx={{
+                                    color: color
+                                }} />
+                        </Box>
+                        :
+                        text
+                }
+            </Button>
+            {loading && (
+                <div
+                    style={{
+                        backgroundColor: 'rgba(255, 255, 255, 0)',
+                        cursor: 'wait',
+                        zIndex: 2,
+                        width: '100%',
+                        height: '100%',
+                        position: 'absolute',
+                    }}
+                />
+            )}
+        </div>
+    )
+}
+
+export default BaseButton;
