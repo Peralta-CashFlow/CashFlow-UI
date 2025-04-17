1312e4b218b2a7ae89bfaf866c61529d373db6b0
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-04-17 15:14:51 -0300

    feat: adding on change event

--- src/components/textfield/BaseTextField.tsx
+++ src/components/textfield/BaseTextField.tsx
@@ -1,61 +1,49 @@
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { TextFieldVariants } from '@mui/material/TextField';
+import TextField, { TextFieldVariants } from '@mui/material/TextField';
 
 interface BaseTextFieldProps {
     label: string;
     color?: string;
     variant?: TextFieldVariants;
     required?: boolean;
-    width?: string;
     type?: string;
+    className?: string;
+    onChange?: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
     fontSize?: string;
-    marginBottom?: string;
 }
 
 const BaseTextField: React.FC<BaseTextFieldProps> = ({
-    label, color = 'white', variant = 'outlined', 
-    required = false, width = '100%', type = 'text',
-    fontSize = '1.2rem', marginBottom = '5%'
+    label, color = 'white', variant = 'outlined',
+    required, type = 'text', className, onChange,
+    fontSize
 }) => {
     return (
-        <Box
-            component="form"
-            autoComplete="off"
-            sx={{ 
-                width: width,
-                display: 'flex',
-                justifyContent: 'center',
-                marginBottom: marginBottom,
+        <TextField
+            label={label}
+            variant={variant}
+            required={required}
+            type={type}
+            className={className}
+            onChange={onChange}
+            sx={{
+                input: { color: color, fontSize: fontSize },
+                label: { color: color },
+                '& .MuiOutlinedInput-root': {
+                    '& fieldset': { borderColor: color },
+                    '&:hover fieldset': { borderColor: color },
+                    '&.Mui-focused fieldset': { borderColor: color },
+                },
             }}
-        >
-            <TextField
-                label={label}
-                variant={variant}
-                required={required}
-                type={type}
-                sx={{
-                    width: width,
-                    input: { color: color, fontSize: fontSize },
-                    label: { color: color },
-                    '& .MuiOutlinedInput-root': {
-                        '& fieldset': { borderColor: color },
-                        '&:hover fieldset': { borderColor: color },
-                        '&.Mui-focused fieldset': { borderColor: color },
-                    },
-                }}
-                slotProps={{
-                    inputLabel: {
-                      sx: {
+            slotProps={{
+                inputLabel: {
+                    sx: {
                         color: color,
                         '&.Mui-focused': {
-                          color: color,
+                            color: color,
                         },
-                      },
                     },
-                  }}
-            />
-        </Box>
+                },
+            }}
+        />
     );
 }
 
