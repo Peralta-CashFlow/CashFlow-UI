f91c31c4b3199e6892e801088a507654a1b475ca
ViniciusPeralta <vinicius-peralta@hotmail.com>
2026-02-04 19:50:47 -0300

    feat: add category page search row

--- src/pages/category/Category.tsx
+++ src/pages/category/Category.tsx
@@ -1,7 +1,13 @@
 import pageStyles from '../Page.module.css'
+import styles from './Category.module.css'
+import colors from '../../assets/colors/colors';
 
 import { useTranslation } from 'react-i18next'
 import BookmarksRoundedIcon from '@mui/icons-material/BookmarksRounded';
+import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
+import BaseTextField from '../../components/textfield/BaseTextField';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import BaseButton from '../../components/button/BaseButton';
 
 const Category: React.FC = () => {
 
@@ -12,6 +18,23 @@ const Category: React.FC = () => {
             <div className={pageStyles.titleBox}>
                 <BookmarksRoundedIcon fontSize='large' />
                 <h1>{t('categories')}</h1>
+            </div>
+            <div className={styles.searchRow}>
+                <div className={styles.searchField}>
+                    <BaseTextField
+                        label={t('search-categories')}
+                        type='text'
+                        initialAdornment={<SearchRoundedIcon />}
+                    />
+                </div>
+                <BaseButton
+                    text={t('create-categorie')}
+                    backGroundColor={colors.lightBlueGreen}
+                    type='button'
+                    fontWeight='bolder'
+                    icon={AddCircleOutlineRoundedIcon}
+                    fontSize='15px'
+                />
             </div>
         </div>
     )
