fc617c314c78982a5802667737f0f36318a277ab
ViniciusPeralta <vinicius-peralta@hotmail.com>
2026-03-25 20:21:45 -0300

    feat: add loading to modal

--- src/components/modal/category/edit/EditCategoryModal.tsx
+++ src/components/modal/category/edit/EditCategoryModal.tsx
@@ -1,7 +1,7 @@
 import styles from './EditCategoryModal.module.css'
 import colors from '../../../../assets/colors/colors';
 
-import { Box, Modal } from "@mui/material";
+import { Box, CircularProgress, Modal } from "@mui/material";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { useCategoryEditionFormik } from "../../../../service/category/form/CategoryEditionForm";
 import CategoryService from "../../../../service/category/CategoryService";
@@ -105,108 +105,120 @@ const EditCategoryModal: React.FC<EditCategoryModalProps> = ({
             <Box
                 className={styles.modal}
             >
-                <div className={styles.header}>
-                    <h1>{t('edit-category')}</h1>
-                </div>
-                <form
-                    onSubmit={editCategoryFormik.handleSubmit}
-                >
-                    <div className={styles.container}>
-                        <div className={styles.left}>
-                            <div className={styles.name}>
-                                <BaseTextField
-                                    label={t('name')}
-                                    fieldName='name'
-                                    required={true}
-                                    value={editCategoryFormik.values.name}
-                                    onBlur={editCategoryFormik.handleBlur}
-                                    onChange={editCategoryFormik.handleChange}
-                                    error={editCategoryFormik.touched.name && Boolean(editCategoryFormik.errors.name)}
-                                    helperText={editCategoryFormik.touched.name && editCategoryFormik.errors.name}
-                                />
-                            </div>
-
-                            <div className={styles.icon}>
-                                <BaseEmojiPicker
-                                    emoji={editCategoryFormik.values.icon}
-                                    setEmoji={(emoji: string | undefined) => editCategoryFormik.setFieldValue('icon', emoji)}
-                                    fontSize={30}
-                                />
-                            </div>
-
-                            <BaseColorPicker
-                                color={editCategoryFormik.values.color}
-                                onChange={(newColor: string | undefined) => editCategoryFormik.setFieldValue('color', newColor)}
-                                label={t('color')}
-                                height='130px'
-                                width='130px'
-                            />
-                        </div>
-
-                        <div className={styles.right}>
-                            <div className={styles.tableWrapper}>
-                                <BaseTable
-                                    headers={tableHeaders}
-                                    rows={editCategoryFormik.values.tags}
-                                    rowKey={'id'}
-                                    width='70%'
-                                    height='100%'
-                                    overflow='auto'
-                                    noDataFoundText={t('category-without-tags')}
-                                    pagination={false}
-                                    headerBackGroundColor={colors.gray}
-                                    headerFontColor={colors.white}
-                                    headerFontSize='20px'
-                                    rowBackGroundColor={colors.lighterGray}
-                                    rowFontColor={colors.white}
-                                    rowFontSize='15px'
-                                    borderColor='black'
-                                    paperBackground={colors.lightGray}
-                                    emptyFontSize='9px'
-                                    canAddRow={isTagListFull}
-                                    addRowAction={addTag}
-                                    canDeleteRow={true}
-                                    deleteRowAction={(index: number) => removeTag(index)}
-                                    editableFields={[
-                                        {
-                                            fieldName: 'name',
-                                            fieldMaxLength: 20,
-                                            onChange: (e, rowIndex) => {
-                                                editCategoryFormik.setFieldValue(
-                                                    `tags[${rowIndex}].name`,
-                                                    e.target.value
-                                                );
-                                            },
-                                            helperText: defineHelperText
-                                        }
-                                    ]}
-                                />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={styles.buttonContainer}>
-                        <BaseButton
-                            text={t('save')}
-                            backGroundColor={colors.lightBlueGreen}
-                            fontSize='100%'
-                            type='submit'
-                            fontWeight='bolder'
-                            loading={loading}
-                            spinnerSize={20}
-                            icon={SaveIcon}
+                {loading ?
+                    <div className={styles.loading}>
+                        <CircularProgress
+                            sx={{
+                                color: colors.white
+                            }}
+                            size='5%'
                         />
-                        <BaseButton
-                            text={t('cancel')}
-                            backGroundColor={'red'}
-                            fontSize='100%'
-                            fontWeight='bold'
-                            type="button"
-                            icon={CancelIcon}
-                            onClick={() => closeModal()}
-                        />
+                    </div> :
+                    <div>
+                        <div className={styles.header}>
+                            <h1>{t('edit-category')}</h1>
+                        </div>
+                        <form
+                            onSubmit={editCategoryFormik.handleSubmit}
+                        >
+                            <div className={styles.container}>
+                                <div className={styles.left}>
+                                    <div className={styles.name}>
+                                        <BaseTextField
+                                            label={t('name')}
+                                            fieldName='name'
+                                            required={true}
+                                            value={editCategoryFormik.values.name}
+                                            onBlur={editCategoryFormik.handleBlur}
+                                            onChange={editCategoryFormik.handleChange}
+                                            error={editCategoryFormik.touched.name && Boolean(editCategoryFormik.errors.name)}
+                                            helperText={editCategoryFormik.touched.name && editCategoryFormik.errors.name}
+                                        />
+                                    </div>
+
+                                    <div className={styles.icon}>
+                                        <BaseEmojiPicker
+                                            emoji={editCategoryFormik.values.icon}
+                                            setEmoji={(emoji: string | undefined) => editCategoryFormik.setFieldValue('icon', emoji)}
+                                            fontSize={30}
+                                        />
+                                    </div>
+
+                                    <BaseColorPicker
+                                        color={editCategoryFormik.values.color}
+                                        onChange={(newColor: string | undefined) => editCategoryFormik.setFieldValue('color', newColor)}
+                                        label={t('color')}
+                                        height='130px'
+                                        width='130px'
+                                    />
+                                </div>
+
+                                <div className={styles.right}>
+                                    <div className={styles.tableWrapper}>
+                                        <BaseTable
+                                            headers={tableHeaders}
+                                            rows={editCategoryFormik.values.tags}
+                                            rowKey={'id'}
+                                            width='70%'
+                                            height='100%'
+                                            overflow='auto'
+                                            noDataFoundText={t('category-without-tags')}
+                                            pagination={false}
+                                            headerBackGroundColor={colors.gray}
+                                            headerFontColor={colors.white}
+                                            headerFontSize='20px'
+                                            rowBackGroundColor={colors.lighterGray}
+                                            rowFontColor={colors.white}
+                                            rowFontSize='15px'
+                                            borderColor='black'
+                                            paperBackground={colors.lightGray}
+                                            emptyFontSize='9px'
+                                            canAddRow={isTagListFull}
+                                            addRowAction={addTag}
+                                            canDeleteRow={true}
+                                            deleteRowAction={(index: number) => removeTag(index)}
+                                            editableFields={[
+                                                {
+                                                    fieldName: 'name',
+                                                    fieldMaxLength: 20,
+                                                    onChange: (e, rowIndex) => {
+                                                        editCategoryFormik.setFieldValue(
+                                                            `tags[${rowIndex}].name`,
+                                                            e.target.value
+                                                        );
+                                                    },
+                                                    helperText: defineHelperText
+                                                }
+                                            ]}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={styles.buttonContainer}>
+                                <BaseButton
+                                    text={t('save')}
+                                    backGroundColor={colors.lightBlueGreen}
+                                    fontSize='100%'
+                                    type='submit'
+                                    fontWeight='bolder'
+                                    loading={loading}
+                                    spinnerSize={20}
+                                    icon={SaveIcon}
+                                />
+                                <BaseButton
+                                    text={t('cancel')}
+                                    backGroundColor={'red'}
+                                    fontSize='100%'
+                                    fontWeight='bold'
+                                    type="button"
+                                    icon={CancelIcon}
+                                    onClick={() => closeModal()}
+                                />
+                            </div>
+                        </form>
                     </div>
-                </form>
+                }
 
                 <div
                     className={styles.close}
